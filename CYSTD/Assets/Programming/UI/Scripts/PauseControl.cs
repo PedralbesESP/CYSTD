acac08a58cded0a4d1449194e73faf74257141d3
AsmaeCharr <asmaecharroudd@gmail.com>
2023-12-15 12:36:24 +0100

    Added pause menu when pressing Esc or P

--- CYSTD/Assets/Programming/UI/Scripts/PauseControl.cs
+++ CYSTD/Assets/Programming/UI/Scripts/PauseControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PauseControl : MonoBehaviour
 {
+    public GameObject PauseMenu;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,30 @@ public class PauseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                PauseMenu.SetActive(true);
+                isPaused = true;
+
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (isPaused)
+            {
+                Resume();
+            }
+        }   
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
