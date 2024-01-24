3256a7dde0a25bd1a1f5003404b85fc8a0851544
Joel Marin <a23joemarllu@inspedralbes.cat>
2024-01-24 18:43:38 +0100

    main fixes

--- CYSTD/Assets/Programming/GameSystems/Scripts/Timer.cs
+++ CYSTD/Assets/Programming/GameSystems/Scripts/Timer.cs
@@ -9,7 +9,8 @@ public class Timer : MonoBehaviour
     [SerializeField]
     float _secondsToEnd;
     [SerializeField]
-    TMP_Text _timerText;
+    TMP_Text _timerText; 
+    bool _last10Seconds = false;
 
     public static Timer Instance;
 
@@ -22,6 +23,15 @@ public class Timer : MonoBehaviour
     void Update()
     {
         _secondsToEnd -= Time.deltaTime;
+        if (_secondsToEnd < 30)
+        {
+            _timerText.color = Color.red;
+        }
+        if (_secondsToEnd < 10 && !_last10Seconds)
+        {
+            _last10Seconds = true;
+            StartCoroutine(Last10SecondsRoutine());
+        }
         if (_secondsToEnd < 0)
         {
             _secondsToEnd = 0;
@@ -43,11 +53,30 @@ public class Timer : MonoBehaviour
         _timerText.SetText(time.ToString(@"hh\:mm\:ss"));
     }
 
+    IEnumerator Last10SecondsRoutine()
+    {
+        while (_last10Seconds && _secondsToEnd >= 1)
+        {
+            _timerText.enabled = !_timerText.enabled;
+            yield return new WaitForSecondsRealtime(0.3f);
+        }
+        _timerText.enabled = true;
+    }
+
+
     public void AddTime(float secondsToAdd)
     {
         if (_secondsToEnd > 0)
         {
             _secondsToEnd += secondsToAdd;
+            if (_secondsToEnd >= 10)
+            {
+                _last10Seconds = true;
+            }
+            if (_secondsToEnd >= 30)
+            {
+                _timerText.color = Color.white;
+            }
         }
     }
 }
