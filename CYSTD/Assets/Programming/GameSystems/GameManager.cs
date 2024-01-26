37117df0e5ec64775f39edbdc2536a54423a57e9
Mae <a21asmchaben@inspedralbes.cat>
2024-01-26 16:52:05 +0100

    deleted sceneloader on game over

--- CYSTD/Assets/Programming/GameSystems/GameManager.cs
+++ CYSTD/Assets/Programming/GameSystems/GameManager.cs
@@ -41,7 +41,6 @@ public class GameManager : MonoBehaviour
 
     public void Loose()
     {
-        SceneLoader.Instance.LoadScene("UI");
         GameOver.SetActive(true);
         DisableInputs();
     }
